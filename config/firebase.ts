081b6bd720200b0679ebafaa117b3cc3ead92633
basuabhirup <basu.abhirup27@gmail.com>
2024-08-31 17:30:37 +0530

    Add firebase config to the project

---
+++ config/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAK84AoIMXm84IhJNcFWPCdvlzNcfgkXJE",
+  authDomain: "hs-ai-flashcard.firebaseapp.com",
+  projectId: "hs-ai-flashcard",
+  storageBucket: "hs-ai-flashcard.appspot.com",
+  messagingSenderId: "602029470080",
+  appId: "1:602029470080:web:d083d0b3c4957b70409165",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
